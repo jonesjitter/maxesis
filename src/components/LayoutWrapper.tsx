f7d7632c5c812888485a078672ca652bed37f722
jonesjitter <jonas@single.dk>
2026-02-04 23:00:42 +0100

    Make overlay background transparent for OBS

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -1,11 +1,24 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 import Navigation from './Navigation';
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
   const isOverlay = pathname === '/overlay';
+
+  // Make body transparent for overlay page
+  useEffect(() => {
+    if (isOverlay) {
+      document.body.style.background = 'transparent';
+      document.documentElement.style.background = 'transparent';
+    }
+    return () => {
+      document.body.style.background = '';
+      document.documentElement.style.background = '';
+    };
+  }, [isOverlay]);
 
   if (isOverlay) {
     return <>{children}</>;
